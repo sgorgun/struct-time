896e9e2f6850d896d16009efc50d544c5b4e055d
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-26 10:43:48 +0000

    Replace TimeTests.cs

--- TimeStruct.Tests/TimeTests.cs
+++ TimeStruct.Tests/TimeTests.cs
@@ -1,36 +1,43 @@
 using NUnit.Framework;
 
+#pragma warning disable CA1707
+
 namespace TimeStruct.Tests
 {
     [TestFixture]
     public class TimeTests
     {
-        [Test, TestCaseSource(typeof(TestCasesSource),nameof(TestCasesSource.TestCasesForToString))]
+        [Test]
+        [TestCaseSource(typeof(TestCasesSource), nameof(TestCasesSource.TestCasesForToString))]
         public void ToStringTests(Time time, string str)
         {
             Assert.AreEqual(str, time.ToString());
         }
 
-        [Test, TestCaseSource(typeof(TestCasesSource),nameof(TestCasesSource.TestCasesForMinutesProperty))]
+        [Test]
+        [TestCaseSource(typeof(TestCasesSource), nameof(TestCasesSource.TestCasesForMinutesProperty))]
         public void MinutesPropertyTests(Time time, int minutes)
         {
             Assert.AreEqual(minutes, time.Minutes);
         }
         
-        [Test, TestCaseSource(typeof(TestCasesSource),nameof(TestCasesSource.TestCasesForHoursProperty))]
+        [Test]
+        [TestCaseSource(typeof(TestCasesSource), nameof(TestCasesSource.TestCasesForHoursProperty))]
         public void HoursPropertyTests(Time time, int hour)
         {
             Assert.AreEqual(hour, time.Hours);
         }
         
-        [Test, TestCaseSource(typeof(TestCasesSource),nameof(TestCasesSource.TestCasesForTwoParametersCtor))]
+        [Test]
+        [TestCaseSource(typeof(TestCasesSource), nameof(TestCasesSource.TestCasesForTwoParametersCtor))]
         public void CtorWithTwoParametersTests(Time time, int hour, int minutes)
         {
             Assert.AreEqual(hour, time.Hours);
             Assert.AreEqual(minutes, time.Minutes);
         }
         
-        [Test, TestCaseSource(typeof(TestCasesSource),nameof(TestCasesSource.TestCasesForOneParameterCtor))]
+        [Test]
+        [TestCaseSource(typeof(TestCasesSource), nameof(TestCasesSource.TestCasesForOneParameterCtor))]
         public void CtorWithOneParameterTests(Time time, int hour, int minutes)
         {
             Assert.AreEqual(hour, time.Hours);
