9f7db6ae85535f837e55c69b7aee7b89a1dcf8a9
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-30 14:41:44 +0000

    Replace Time.cs

--- TimeStruct/Time.cs
+++ TimeStruct/Time.cs
@@ -1,4 +1,36 @@
+#pragma warning disable CA1815
+
 namespace TimeStruct
 {
-   
+    public readonly struct Time
+    {
+        private const int MinutesInHour = 60;
+        private const int HourInDay = 24;
+        
+        public Time(int minutes)
+            : this(0, minutes)
+        {
+        }
+
+        public Time(int hours, int minutes = 0)
+            : this()
+        {
+            this.Hours = Mod((hours * MinutesInHour + minutes) / (double) MinutesInHour, HourInDay);
+            this.Minutes = Mod(minutes, MinutesInHour);
+        }
+
+        public int Hours { get; }
+
+        public int Minutes { get; }
+
+        public void Deconstruct(out int hours, out int minutes)
+        {
+            hours = this.Hours;
+            minutes = this.Minutes;
+        }
+
+        public new string ToString() => $"{Hours:00}:{Minutes:00}";
+
+        private static int Mod(double x, double y) => (int)((x % y + y) % y);
+    }
 }
